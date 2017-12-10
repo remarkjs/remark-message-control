ede22b1c03ad83e9ddca6b41bb8f0392e0ee0823
Titus Wormer <tituswormer@gmail.com>
2017-12-10 12:51:37 +0100

    Refactor core

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-/* Dependencies. */
 var trim = require('trim');
-var vfileLocation = require('vfile-location');
+var location = require('vfile-location');
 var visit = require('unist-util-visit');
 var marker = require('mdast-comment-marker');
 
@@ -13,11 +12,9 @@ var ALLOWED_VERBS = {
   ignore: true
 };
 
-/* Expose. */
-module.exports = attacher;
+module.exports = messageControl;
 
-/* Filter. */
-function attacher(options) {
+function messageControl(options) {
   var name = options && options.name;
   var sources;
   var known;
@@ -43,21 +40,111 @@ function attacher(options) {
 
   return transformer;
 
-  function transformer(ast, file) {
-    var location = vfileLocation(file);
+  function transformer(tree, file) {
+    var toOffset = location(file).toOffset;
     var initial = !reset;
-    var gaps = detectGaps(ast, file);
+    var gaps = detectGaps(tree, file);
     var scope = {};
     var globals = [];
 
-    /**
-     * Helper to check (and possibly warn) if a ruleId
-     * is unknown.
-     *
-     * @param {string} ruleId - Identifier.
-     * @param {string} verb - Thing to do with `ruleId`.
-     * @param {Position} pos - Position of marker.
-     */
+    visit(tree, 'html', visitor);
+
+    file.messages = file.messages.filter(filter);
+
+    function visitor(node, position, parent) {
+      var mark = marker(node);
+      var ruleIds;
+      var ruleId;
+      var verb;
+      var index;
+      var length;
+      var next;
+      var pos;
+      var tail;
+
+      if (!mark || mark.name !== options.name) {
+        return;
+      }
+
+      ruleIds = mark.attributes.split(/\s/g);
+      verb = ruleIds.shift();
+      next = parent.children[position + 1];
+      pos = mark.node.position && mark.node.position.start;
+      tail = next && next.position && next.position.end;
+
+      if (!verb || !ALLOWED_VERBS[verb] === true) {
+        file.fail(
+          'Unknown keyword `' + verb + '`: expected ' +
+          '`\'enable\'`, `\'disable\'`, or `\'ignore\'`',
+          mark.node
+        );
+      }
+
+      length = ruleIds.length;
+      index = -1;
+
+      while (++index < length) {
+        ruleId = ruleIds[index];
+
+        if (isKnown(ruleId, verb, mark.node)) {
+          toggle(pos, verb === 'enable', ruleId);
+
+          if (verb === 'ignore') {
+            toggle(tail, true, ruleId);
+          }
+        }
+      }
+
+      /* Apply to all rules. */
+      if (!length) {
+        if (verb === 'ignore') {
+          toggle(pos, false);
+          toggle(tail, true);
+        } else {
+          toggle(pos, verb === 'enable');
+          reset = verb !== 'enable';
+        }
+      }
+    }
+
+    function filter(message) {
+      var gapIndex = gaps.length;
+      var ruleId = message.ruleId;
+      var ranges = scope[ruleId];
+      var pos;
+
+      /* Keep messages from a different source. */
+      if (!message.source || sources.indexOf(message.source) === -1) {
+        return true;
+      }
+
+      /* We only ignore messages if they‘re disabled,
+       * *not* when they’re not in the document. */
+      if (!message.line) {
+        message.line = 1;
+      }
+
+      if (!message.column) {
+        message.column = 1;
+      }
+
+      /* Check whether the warning is inside a gap. */
+      pos = toOffset(message);
+
+      while (gapIndex--) {
+        if (
+          gaps[gapIndex].start <= pos &&
+          gaps[gapIndex].end > pos
+        ) {
+          return false;
+        }
+      }
+
+      /* Check whether allowed by specific and global states. */
+      return check(message, ranges, ruleId) && check(message, globals);
+    }
+
+    /* Helper to check (and possibly warn) if a ruleId is unknown. */
     function isKnown(ruleId, verb, pos) {
       var result = known ? known.indexOf(ruleId) !== -1 : true;
 
@@ -68,8 +155,7 @@ function attacher(options) {
       return result;
     }
 
-    /* Get the latest state of a rule.
-     * When without `ruleId`, gets global state. */
+    /* Get the latest state of a rule. When without `ruleId`, gets global state. */
     function getState(ruleId) {
       var ranges = ruleId ? scope[ruleId] : globals;
 
@@ -114,62 +200,6 @@ function attacher(options) {
       }
     }
 
-    visit(ast, 'html', function (node, position, parent) {
-      var mark = marker(node);
-      var ruleIds;
-      var ruleId;
-      var verb;
-      var index;
-      var length;
-      var next;
-      var pos;
-      var tail;
-
-      if (!mark || mark.name !== options.name) {
-        return;
-      }
-
-      ruleIds = mark.attributes.split(/\s/g);
-      verb = ruleIds.shift();
-      next = parent.children[position + 1];
-      pos = mark.node.position && mark.node.position.start;
-      tail = next && next.position && next.position.end;
-
-      if (!verb || !ALLOWED_VERBS[verb] === true) {
-        return file.fail(
-          'Unknown keyword `' + verb + '`: expected ' +
-          '`\'enable\'`, `\'disable\'`, or `\'ignore\'`',
-          mark.node
-        );
-      }
-
-      length = ruleIds.length;
-      index = -1;
-
-      while (++index < length) {
-        ruleId = ruleIds[index];
-
-        if (isKnown(ruleId, verb, mark.node)) {
-          toggle(pos, verb === 'enable', ruleId);
-
-          if (verb === 'ignore') {
-            toggle(tail, true, ruleId);
-          }
-        }
-      }
-
-      /* Apply to all rules. */
-      if (!length) {
-        if (verb === 'ignore') {
-          toggle(pos, false);
-          toggle(tail, true);
-        } else {
-          toggle(pos, verb === 'enable');
-          reset = verb !== 'enable';
-        }
-      }
-    });
-
     /* Check all `ranges` for `message`. */
     function check(message, ranges, id) {
       /* Check the state at the message's position. */
@@ -181,20 +211,13 @@ function attacher(options) {
         range = ranges[index];
 
         /* istanbul ignore if - generated marker. */
-        if (
-          !range.position ||
-          !range.position.line ||
-          !range.position.column
-        ) {
+        if (!range.position || !range.position.line || !range.position.column) {
           continue;
         }
 
         if (
           range.position.line < message.line ||
-          (
-            range.position.line === message.line &&
-            range.position.column < message.column
-          )
+          (range.position.line === message.line && range.position.column < message.column)
         ) {
           return range.state === true;
         }
@@ -208,61 +231,18 @@ function attacher(options) {
 
       return reset ? enable.indexOf(id) !== -1 : disable.indexOf(id) === -1;
     }
-
-    file.messages = file.messages.filter(function (message) {
-      var gapIndex = gaps.length;
-      var ruleId = message.ruleId;
-      var ranges = scope[ruleId];
-      var pos;
-
-      /* Keep messages from a different source. */
-      if (!message.source || sources.indexOf(message.source) === -1) {
-        return true;
-      }
-
-      /* We only ignore messages if they‘re disabled,
-       * *not* when they’re not in the document. */
-      if (!message.line) {
-        message.line = 1;
-      }
-
-      if (!message.column) {
-        message.column = 1;
-      }
-
-      /* Check whether the warning is inside a gap. */
-      pos = location.toOffset(message);
-
-      while (gapIndex--) {
-        if (
-          gaps[gapIndex].start <= pos &&
-          gaps[gapIndex].end > pos
-        ) {
-          return false;
-        }
-      }
-
-      /* Check whether allowed by specific and global states. */
-      return check(message, ranges, ruleId) && check(message, globals);
-    });
   }
 }
 
-/**
- * Detect gaps in `ast`.
- *
- * @param {Node} ast - Syntax tree.
- * @param {VFile} file - Virtual file.
- * @return {Array.<Object>} - Gaps.
- */
-function detectGaps(ast, file) {
-  var lastNode = ast.children[ast.children.length - 1];
+/* Detect gaps in `ast`. */
+function detectGaps(tree, file) {
+  var lastNode = tree.children[tree.children.length - 1];
   var offset = 0;
   var isGap = false;
   var gaps = [];
 
   /* Find all gaps. */
-  visit(ast, one);
+  visit(tree, one);
 
   /* Get the end of the document.
    * This detects if the last node was the last node.
@@ -278,10 +258,10 @@ function detectGaps(ast, file) {
     update();
 
     update(
-      ast &&
-      ast.position &&
-      ast.position.end &&
-      ast.position.end.offset - 1
+      tree &&
+      tree.position &&
+      tree.position.end &&
+      tree.position.end.offset - 1
     );
   }
 
